7293cc398c1af18dfa2f0d9a581f16bf18c914ec
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-16 22:51:03 +0700

    amr_jsonrpc 13.0.0.0.0

---
+++ amr_jsonrpc/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "JSON RPC",
+    'summary': """Libray""",
+    'description': """JSON RPC library""",
+    'author': "Agus Muhammad Ramdan",
+    "license": "AGPL-3",
+    'website': "http://agus.ramdan.tech",
+    'category': 'tools',
+    'version': '13.0.0.0.0',
+    'depends': ['base', ],
+}
