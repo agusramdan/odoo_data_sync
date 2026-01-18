060925b29395d85be95a92f3a6d65e45516d5eea
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-18 21:52:48 +0700

    amr_data_sync_hr

---
+++ amr_data_sync_hr/models/hr_departement.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class HrDepartment(models.Model):
+    _name = "hr.department"
+    _inherit = [_name, 'internal.data.default.mixin']
