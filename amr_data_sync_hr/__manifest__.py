3c3e7ea5d741c0ba3a8d6d8396d69e81b6153710
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-18 21:52:48 +0700

    amr_data_sync_hr

---
+++ amr_data_sync_hr/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "External Data Sync HR ",
+    'summary': """
+        Store information data from external system
+        """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    "license": "AGPL-3",
+    'website': "http://agus.ramdan.tech",
+    'category': 'API',
+    'version': '13.0.0.1.0',
+    'depends': ['base', 'hr', 'amr_data_sync'],
+    # always loaded
+    'data': [],
+}
