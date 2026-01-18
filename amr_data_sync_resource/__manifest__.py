4027b4fb56edc135d967d9bf7b6a1f14d49afdde
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-18 21:54:55 +0700

    amr_data_sync_resource

---
+++ amr_data_sync_resource/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "External Data Sync Resource ",
+    'summary': """
+        Store information data from external system
+        """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    "license": "AGPL-3",
+    'website': "http://agus.ramdan.tech",
+    'category': 'API',
+    'version': '13.0.0.1.0',
+    'depends': ['base', 'resource', 'amr_data_sync'],
+    # always loaded
+    'data': [],
+}
