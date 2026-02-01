fe9e4ff1d984a80728f325347ae37da970c82dd2
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-02-01 10:45:36 +0700

    amr_data_sync_event 13.0.0.0.2

---
+++ amr_data_sync_event/models/data_sync_cron.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+from odoo import models, fields, api, _
+import traceback
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ExternalDataSyncCron(models.Model):
+    _inherit = 'external.data.sync.cron'
+    _description = """Model strategy bagaimana object di sync cron dari server external
+    """
+    sync_strategy_id = fields.Many2one(
+        'external.data.sync.strategy',
+        string='Parent Data Sync Strategy',
+        delegate=True, ondelete='restrict', required=True
+    )
+
+    active = fields.Boolean(default=True)
+    # sync_cron = fields.Boolean(default=True)
+    next_sync_datetime = fields.Datetime()
+    last_sync_datetime = fields.Datetime()
+
+    @api.model_create_multi
+    @api.returns('self', lambda value: value.id)
+    def create(self, vals_list):
+        return super(ExternalDataSyncCron, self.with_context(__from_sync_cron=True)).create(vals_list)
+
+    def write(self, vals):
+        result = super(ExternalDataSyncCron, self).write(vals)
+        if self._context.get('__from_sync_cron'):
+            return result
+        if 'active' in vals:
+            for rec in self.with_context(__from_sync_cron=True):
+                if rec.sync_strategy_id and rec.active != rec.sync_strategy_id.sync_cron:
+                    rec.sync_strategy_id.write({
+                        'sync_cron': rec.active
+                    })
+        return result
+
+    def action_sync_now(self):
+        self.sync_strategy_id.action_sync_now()
+
+    def cron_sync_from_server(self):
+        data_sync_models = self or self.search([
+            ('server_sync_id.event_listener','=',False),
+            ('strategy', 'in', ['external_cud', 'external_cu', 'external_create']),
+            '|', ('next_sync_datetime', '=', False), ('next_sync_datetime', '<=', fields.Datetime.now())
+        ], order='next_sync_datetime asc')
+        super(ExternalDataSyncCron,data_sync_models).cron_sync_from_server()
