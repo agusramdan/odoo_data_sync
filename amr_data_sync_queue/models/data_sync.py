ae2be1d639d7a0d74b3b19743b4870a72cdeb702
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-06-16 18:42:08 +0700

    amr_data_sync_queue

--- amr_data_sync_queue/models/data_sync.py
+++ amr_data_sync_queue/models/data_sync.py
@@ -12,13 +12,14 @@ from odoo.tools import date_utils
 from psycopg2.errors import SerializationFailure
 from odoo.addons.queue_job.exception import RetryableJobError
 from psycopg2._psycopg import TransactionRollbackError
+
 _logger = logging.getLogger(__name__)
 
 
 class ExternalDataSync(models.Model):
     _inherit = 'external.data.sync'
 
-    on_queue = fields.Boolean(default=False,)
+    on_queue = fields.Boolean(default=False, )
 
     def process_with_handel_error(self):
         try:
@@ -41,30 +42,28 @@ class ExternalDataSync(models.Model):
         self.write({'on_queue': False})
         return result
 
-    def write_error_safe(self,error_data,using_pool=False):
-        error_data['on_queue']=False
-        super().write_error_safe(error_data,using_pool=using_pool)
+    def write_error_safe(self, error_data, using_pool=False):
+        error_data['on_queue'] = False
+        super().write_error_safe(error_data, using_pool=using_pool)
 
-    def dispatch_process(self,run_immediate=False):
-        self.write({'on_queue': True,'state':'process'})
+    def dispatch_process(self, run_immediate=False):
+        self.write({'on_queue': True, 'state': 'process'})
         self.with_delay().process_with_handel_error()
 
     def cron_process_data(self, limit=1000):
-        records  = self.search(
-            [('on_queue','=',False),('need_get_data_json', '=', True)],
+        records = self.search(
+            [('on_queue', '=', False), ('need_get_data_json', '=', True)],
             limit=limit, order='next_processing_datetime asc,last_processing_datetime asc, id '
         )
-        for rec in records :
+        for rec in records:
             rec.dispatch_process()
 
-        records =self.search(
-            [('on_queue','=',False),('state', '!=', 'done'),
+        records = self.search(
+            [('on_queue', '=', False), ('state', '!=', 'done'),
              '|',
              ('next_processing_datetime', '<=', fields.Datetime.now()),
              ('next_processing_datetime', '=', False)],
             limit=limit, order='next_processing_datetime asc,last_processing_datetime asc, id '
         )
-        for rec in records :
+        for rec in records:
             rec.dispatch_process()
-
-
